ce4a00303980531359a6000f0b6d020315072bfb
rami <rami@example.com>
2026-09-04 20:10:50 +0100

    premier commit de projet grocery store

---
+++ src/main/java/com/dreamTeam/backGroceryStore/BackGroceryStoreApplication.java
@@ -0,0 +1,13 @@
+package com.dreamTeam.backGroceryStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGroceryStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGroceryStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamTeam/backGroceryStore/BackGroceryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamTeam.backGroceryStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackGroceryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
